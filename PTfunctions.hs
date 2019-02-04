5c1125beb680879eea64418bf4fc65ac56012d93
elEdlamo05 <eledlamo05@hanovernorwichschools.org>
2019-02-04 15:03:25 +0000

    PTfunctions

--- PTfunctions.hs
+++ PTfunctions.hs
@@ -3,9 +3,9 @@ quadraticVertex a b c = -b/(2*a)
 quadraticDiscriminant a b c = (b**2) - 4*a*c
 quadraticRealSolutionQty a b c = if (quadraticDiscriminant a b c) >= 0
     then if (quadraticDiscriminant a b c) == 0
-            then print("There is one Solution")
-        else print("There are two Solutions")
-    else print("There are no Solutions")
+            then 1
+        else 2
+    else 0
 convertFtoC f = (f - 32)/1.8
 convertCtoF c = (1.8*c) + 32
 convertTemp t s = if s == "c"
